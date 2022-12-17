f75460878f61bb1bee7ea68ae3d8518403da0dd4
shinaeli <omotoshoelisha@gmail.com>
2022-12-17 23:53:51 +0100

    erased a blank line

--- edabit.js
+++ edabit.js
@@ -197,7 +197,6 @@ class Relative extends Person {
 	}
 }
 
-
 // A boomerang is a V-shaped sequence that is either upright or upside down. 
 // Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
 // Some boomerang examples:
